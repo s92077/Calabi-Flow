67fb338ba1afb7c5e7cc5a9af5208dc8bc0689e1
s92077 <s92077@msn.com>
2020-01-04 17:00:07 +0800

    -fix some problems\n delete model files

--- main.m
+++ main.m
@@ -6,25 +6,25 @@ V=Ho.V;
 % PlotMesh(M.F, M.V);
 % [F,V,extra] = read_obj('hemisphere.obj');
 
-delta=0.0001;
+delta=0.00001;
 
 Vno=size(V,1);
 [VB, VI, VBno] = BoundaryIndex(F);
 E = sparse(F, F(:,[2,3,1]), true, Vno, Vno);
-% [I, R] = InvDistMetric(F, V, VB);
+[I, R] = InvDistMetric(F, V, VB);
 % [I, R] = ThurstonMetric(F, V, VB);
 % [I, R] = TangentMetric(F, Vno);
-[I, R] = IsoscelesConstantAngleMetric(F, V, pi/2);
+% [I, R] = IsoscelesConstantAngleMetric(F, V, pi/2);
 U=log(R);
 iter=1;
 while true
     L = CalculateLength(F, V, R, I);
     G = GaussianCurvature(F, V, L, VB, VBno);
 %     G = GaussianCurvatureTangent(F, R, Vno, VB, VBno);
-    Gbar = zeros(size(G));
-    Gbar(VB,1) = 2*pi / VBno;
+%     Gbar = zeros(size(G));
+%     Gbar(VB,1) = 2*pi / VBno;
 %     Gbar([45,46,1,82])=pi/2;
-%     Gbar = TargetCurvature(F, V, L, VB);
+    Gbar = TargetCurvature(F, V, L, VB);
     fprintf('iter: %d %.9f\n',iter, max(abs(G-Gbar)));
     iter=iter+1;
     if max(abs(G-Gbar))<5e-8
@@ -37,7 +37,7 @@ while true
 %     EW=EW+delta*speye(size(EW));
 %     EW(sub2ind([Vno,Vno],1:Vno,1:Vno)) = EW(sub2ind([Vno,Vno],1:Vno,1:Vno))+1;
     U = U + EW\( Gbar - G );
-%     U = U + Ldual * ( Gbar - G );
+%     U = U + delta * EW * ( Gbar - G );
     U = U - sum(U) / Vno;
     R = exp(U);
 %     VL = Embedding(F, V, VB, L);
@@ -49,6 +49,13 @@ end
 Z=euclidean_embed(F,U,I);
 UV=[real(Z), imag(Z)];
 PlotMesh(F,UV);
+
+% figure
+% subplot(1,2,1)
+% PlotMesh(Ho.F, Ho.V, Ho.Vrgb);
+% subplot(1,2,2)
+% PlotMesh(F, UV, Ho.Vrgb);
+
 AreaA=sum(abs(cross(reshape(UV(F,1),[size(F,1),3]),reshape(UV(F,2),[size(F,1),3]))),2);
 AreaB=sum(cross(V(F(:,1),:)-V(F(:,2),:),V(F(:,1),:)-V(F(:,3),:)).^2,2);
 
